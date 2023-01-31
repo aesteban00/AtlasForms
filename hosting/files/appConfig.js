6fc7fd83440eaf21c4a0e9256223392f2f61a4a0
Angel Esteban <47087658+aesteban00@users.noreply.github.com>
2023-01-31 15:42:31 +0100

    version 1

--- hosting/files/appConfig.js
+++ hosting/files/appConfig.js
@@ -1,7 +1,7 @@
 //Change this so your browser app knows what atlas app services to connect to
 
 
-const atlasAppConfig = { ATLAS_SERVICES_APPID : "atlasforms-atlasforms-edmpk" }
+const atlasAppConfig = { ATLAS_SERVICES_APPID : "atlasforms-ycyof" }
 
 
 //All the strings that aren't in HTML to allow localization
